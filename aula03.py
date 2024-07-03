7248be0b654b4792da775d5f317afd54625d7853
GabrielPadilha <gabrielpadilha1705@gmail.com>
2024-07-03 16:55:00 -0300

    Versão 1.2

---
+++ aula03.py
@@ -0,0 +1,18 @@
+"""
+var_inicial = "açúcar"
+print(var_inicial) 
+
+if 5 > 2:
+ print("Five is greater than two!") 
+ if "a" == "a":
+   print("igualzinho")
+    
+
+else:
+   print("Falso")
+"""
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
